6c2eeedeff8f316c80863026182876a8e97bf807
Andrew Peck <anp@ucla.edu>
2016-03-08 16:07:04 -0800

    Update the GEM Fiber Input mon_in flag to accept the k-chars used in the new data protocol

--- source/gem/gtx_gem_fiber_in.v
+++ source/gem/gtx_gem_fiber_in.v
@@ -280,13 +280,15 @@ module gtx_gem_fiber_in
         else begin
         if(CEW0)    begin     // this gets set for the first time after the first CEW3
             lt_trg_reg <= lt_trg;
-            mon_in[0] <= (!gem_rx_lossofsync[1]) && (gem_rx_isk==2'b01) && (gem_rx_notintable[1:0]==2'b00) && (gem_rx_data[4:1] == 4'hE); // allows 8 possible EOF markers:
-                // 1C,3C,5C,7C,9C,BC,DC,FD are valid K-words available to represent 3 extra bits in the data stream. --Skip F7, FB, FC and FE.
-                // So we could identify that BC=0 (very common), 1C=1, 3C=2, 5C=3, 7C=4, 9C=5, DC=6, FD=7 (all less common).
-                // Also allows use of 8-bit "data" byte in the EOF frame! Thus we have "59 data bits" per link (i.e. 118) every BX.
-                // Perhaps a bit could mark the BC0, or overflow/error condition, or QPLL lock was lost, etc.
-                w0_reg <= gem_rx_data [15:8];
-                NONZERO_WORD[0] <= |gem_rx_data[15:8];
+            w0_reg <= gem_rx_data [15:8];
+            NONZERO_WORD[0] <= |gem_rx_data[15:8];
+
+            mon_in[0] <= (!gem_rx_lossofsync[1]) && (gem_rx_isk==2'b01) && (gem_rx_notintable[1:0]==2'b00) && ({gem_rx_data[7],gem_rx_data[5:4]}==3'h7); 
+            // allows 8 possible EOF markers:
+            // 1C,3C,5C,7C,9C,BC,DC,FD are valid K-words available to represent 3 extra bits in the data stream. --Skip F7, FB, FC and FE.
+            // So we could identify that BC=0 (very common), 1C=1, 3C=2, 5C=3, 7C=4, 9C=5, DC=6, FD=7 (all less common).
+            // Also allows use of 8-bit "data" byte in the EOF frame! Thus we have "59 data bits" per link (i.e. 118) every BX.
+            // Perhaps a bit could mark the BC0, or overflow/error condition, or QPLL lock was lost, etc.
         end
 
         else if(CEW1)    begin
