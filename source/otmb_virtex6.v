a56e1c7f85a67309b26bcef0a7a27be9637d703f
Andrew Peck <anp@ucla.edu>
2016-12-06 19:06:47 -0600

    Fix typo in GEM output rollups

--- source/otmb_virtex6.v
+++ source/otmb_virtex6.v
@@ -1544,21 +1544,117 @@
   wire  [0:0]   gem_vpf3 [MXGEM-1:0]; // cluster3 valid flag
 
   // join fibers together into the two GEM chambers
- wire [13:0] gemA_cluster [7:0] = { gem_cluster3[1], gem_cluster2[1], gem_cluster1[1], gem_cluster0[1], gem_cluster3[0], gem_cluster2[0], gem_cluster1[0], gem_cluster0[0]};
- wire [13:0] gemB_cluster [7:0] = { gem_cluster3[3], gem_cluster2[3], gem_cluster3[3], gem_cluster1[3], gem_cluster3[2], gem_cluster2[2], gem_cluster3[2], gem_cluster1[2]};
+  wire [13:0] gemA_cluster [7:0] = {
+    gem_cluster3[1],
+    gem_cluster2[1],
+    gem_cluster1[1],
+    gem_cluster0[1],
+    gem_cluster3[0],
+    gem_cluster2[0],
+    gem_cluster1[0],
+    gem_cluster0[0]
+  };
 
- wire  [7:0] gemA_strip [7:0] = { gem_strip3[1], gem_strip2[1], gem_strip1[1], gem_strip0[1], gem_strip3[0], gem_strip2[0], gem_strip1[0], gem_strip0[0]};
- wire  [7:0] gemB_strip [7:0] = { gem_strip3[3], gem_strip2[3], gem_strip3[3], gem_strip1[3], gem_strip3[2], gem_strip2[2], gem_strip3[2], gem_strip1[2]};
+  wire [13:0] gemB_cluster [7:0] = {
+    gem_cluster3[3],
+    gem_cluster2[3],
+    gem_cluster1[3],
+    gem_cluster0[3],
+    gem_cluster3[2],
+    gem_cluster2[2],
+    gem_cluster1[2],
+    gem_cluster0[2]
+  };
 
- wire  [7:0] gemA_halfstrip [7:0] = { gem_halfstrip3[1], gem_halfstrip2[1], gem_halfstrip1[1], gem_halfstrip0[1], gem_halfstrip3[0], gem_halfstrip2[0], gem_halfstrip1[0], gem_halfstrip0[0]};
- wire  [7:0] gemB_halfstrip [7:0] = { gem_halfstrip3[3], gem_halfstrip2[3], gem_halfstrip3[3], gem_halfstrip1[3], gem_halfstrip3[2], gem_halfstrip2[2], gem_halfstrip3[2], gem_halfstrip1[2]};
+  wire  [7:0] gemA_strip [7:0] = {
+    gem_strip3[1],
+    gem_strip2[1],
+    gem_strip1[1],
+    gem_strip0[1],
+    gem_strip3[0],
+    gem_strip2[0],
+    gem_strip1[0],
+    gem_strip0[0]
+  };
 
- wire  [2:0] gemA_halfstrip_size [7:0] = { gem_halfstrip_size3[1], gem_halfstrip_size2[1], gem_halfstrip_size1[1], gem_halfstrip_size0[1], gem_halfstrip_size3[0], gem_halfstrip_size2[0], gem_halfstrip_size1[0], gem_halfstrip_size0[0]};
- wire  [2:0] gemB_halfstrip_size [7:0] = { gem_halfstrip_size3[3], gem_halfstrip_size2[3], gem_halfstrip_size3[3], gem_halfstrip_size1[3], gem_halfstrip_size3[2], gem_halfstrip_size2[2], gem_halfstrip_size3[2], gem_halfstrip_size1[2]};
+  wire  [7:0] gemB_strip [7:0] = {
+    gem_strip3[3],
+    gem_strip2[3],
+    gem_strip1[3],
+    gem_strip0[3],
+    gem_strip3[2],
+    gem_strip2[2],
+    gem_strip1[2],
+    gem_strip0[2]
+  };
 
-  wire [7:0] gemA_vpf = {gem_vpf3[1], gem_vpf2[1], gem_vpf1[1], gem_vpf0[1], gem_vpf3[0], gem_vpf2[0], gem_vpf1[0], gem_vpf0[0]};
-  wire [7:0] gemB_vpf = {gem_vpf3[3], gem_vpf2[3], gem_vpf1[3], gem_vpf0[3], gem_vpf3[2], gem_vpf2[2], gem_vpf1[2], gem_vpf0[2]};
-  wire        gem_any = (|gemA_vpf) | (|gemB_vpf);
+  wire  [7:0] gemA_halfstrip [7:0] = {
+   gem_halfstrip3[1],
+   gem_halfstrip2[1],
+   gem_halfstrip1[1],
+   gem_halfstrip0[1],
+   gem_halfstrip3[0],
+   gem_halfstrip2[0],
+   gem_halfstrip1[0],
+   gem_halfstrip0[0]
+  };
+
+  wire  [7:0] gemB_halfstrip [7:0] = {
+   gem_halfstrip3[3],
+   gem_halfstrip2[3],
+   gem_halfstrip1[3],
+   gem_halfstrip0[3],
+   gem_halfstrip3[2],
+   gem_halfstrip2[2],
+   gem_halfstrip1[2],
+   gem_halfstrip0[2]
+  };
+
+  wire  [2:0] gemA_halfstrip_size [7:0] = {
+   gem_halfstrip_size3[1],
+   gem_halfstrip_size2[1],
+   gem_halfstrip_size1[1],
+   gem_halfstrip_size0[1],
+   gem_halfstrip_size3[0],
+   gem_halfstrip_size2[0],
+   gem_halfstrip_size1[0],
+   gem_halfstrip_size0[0]
+  };
+
+  wire  [2:0] gemB_halfstrip_size [7:0] = {
+   gem_halfstrip_size3[3],
+   gem_halfstrip_size2[3],
+   gem_halfstrip_size1[3],
+   gem_halfstrip_size0[3],
+   gem_halfstrip_size3[2],
+   gem_halfstrip_size2[2],
+   gem_halfstrip_size1[2],
+   gem_halfstrip_size0[2]
+  };
+
+  wire [7:0] gemA_vpf = {
+    gem_vpf3[1],
+    gem_vpf2[1],
+    gem_vpf1[1],
+    gem_vpf0[1],
+    gem_vpf3[0],
+    gem_vpf2[0],
+    gem_vpf1[0],
+    gem_vpf0[0]
+  };
+
+  wire [7:0] gemB_vpf = {
+    gem_vpf3[3],
+    gem_vpf2[3],
+    gem_vpf1[3],
+    gem_vpf0[3],
+    gem_vpf3[2],
+    gem_vpf2[2],
+    gem_vpf1[2],
+    gem_vpf0[2]
+  };
+
+  wire gem_any = (|gemA_vpf) | (|gemB_vpf);
 
   wire [3:0] gem_delay;
 
