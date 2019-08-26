6fd9db403333e4156d4e00c5da8a8d2e4e3e2cd1
tahuang1991 <taohuang@phyics.tamu.edu>
2019-08-27 00:29:45 +0200

    try to work with function to get win_pri_best

--- source/tmb/tmb.v
+++ source/tmb/tmb.v
@@ -1049,21 +1049,36 @@
   endgenerate
 
   function [7:0] pri_win_best;
-  input [3:0] win_pri [15:0];
+  input [3:0] win_pri_0;
+  input [3:0] win_pri_1;
+  input [3:0] win_pri_2;
+  input [3:0] win_pri_3;
+  input [3:0] win_pri_4;
+  input [3:0] win_pri_5;
+  input [3:0] win_pri_6;
+  input [3:0] win_pri_7;
+  input [3:0] win_pri_8;
+  input [3:0] win_pri_9;
+  input [3:0] win_pri_10;
+  input [3:0] win_pri_11;
+  input [3:0] win_pri_12;
+  input [3:0] win_pri_13;
+  input [3:0] win_pri_14;
+  input [3:0] win_pri_15;
   wire [0:0] win_s0  [7:0];        // Tree encoder Finds best 4 of 16 window positions
   wire [1:0] win_s1  [3:0];
 
   wire [3:0] pri_s0  [7:0];
   wire [3:0] pri_s1  [3:0];
 
-  assign {pri_s0[7],win_s0[7]} = (win_pri[15] > win_pri[14]) ? {win_pri[15],1'b1} : {win_pri[14],1'b0};
-  assign {pri_s0[6],win_s0[6]} = (win_pri[13] > win_pri[12]) ? {win_pri[13],1'b1} : {win_pri[12],1'b0};
-  assign {pri_s0[5],win_s0[5]} = (win_pri[11] > win_pri[10]) ? {win_pri[11],1'b1} : {win_pri[10],1'b0};
-  assign {pri_s0[4],win_s0[4]} = (win_pri[ 9] > win_pri[ 8]) ? {win_pri[ 9],1'b1} : {win_pri[ 8],1'b0};
-  assign {pri_s0[3],win_s0[3]} = (win_pri[ 7] > win_pri[ 6]) ? {win_pri[ 7],1'b1} : {win_pri[ 6],1'b0};
-  assign {pri_s0[2],win_s0[2]} = (win_pri[ 5] > win_pri[ 4]) ? {win_pri[ 5],1'b1} : {win_pri[ 4],1'b0};
-  assign {pri_s0[1],win_s0[1]} = (win_pri[ 3] > win_pri[ 2]) ? {win_pri[ 3],1'b1} : {win_pri[ 2],1'b0};
-  assign {pri_s0[0],win_s0[0]} = (win_pri[ 1] > win_pri[ 0]) ? {win_pri[ 1],1'b1} : {win_pri[ 0],1'b0};
+  assign {pri_s0[7],win_s0[7]} = (win_pri[15] > win_pri_14 ? {win_pri_15,1'b1} : {win_pri_14,1'b0};
+  assign {pri_s0[6],win_s0[6]} = (win_pri[13] > win_pri_12 ? {win_pri_13,1'b1} : {win_pri_12,1'b0};
+  assign {pri_s0[5],win_s0[5]} = (win_pri[11] > win_pri_10 ? {win_pri_11,1'b1} : {win_pri_10,1'b0};
+  assign {pri_s0[4],win_s0[4]} = (win_pri[ 9] > win_pri_8  ? {win_pri_9 ,1'b1} : {win_pri_8 ,1'b0};
+  assign {pri_s0[3],win_s0[3]} = (win_pri[ 7] > win_pri_6  ? {win_pri_7 ,1'b1} : {win_pri_6 ,1'b0};
+  assign {pri_s0[2],win_s0[2]} = (win_pri[ 5] > win_pri_4  ? {win_pri_5 ,1'b1} : {win_pri_4 ,1'b0};
+  assign {pri_s0[1],win_s0[1]} = (win_pri[ 3] > win_pri_2  ? {win_pri_3 ,1'b1} : {win_pri_2 ,1'b0};
+  assign {pri_s0[0],win_s0[0]} = (win_pri[ 1] > win_pri_0  ? {win_pri_1 ,1'b1} : {win_pri_0 ,1'b0};
 
   assign {pri_s1[3],win_s1[3]} = (pri_s0[7] > pri_s0[6]) ? {pri_s0[7],{1'b1,win_s0[7]}} : {pri_s0[6],{1'b0,win_s0[6]}};
   assign {pri_s1[2],win_s1[2]} = (pri_s0[5] > pri_s0[4]) ? {pri_s0[5],{1'b1,win_s0[5]}} : {pri_s0[4],{1'b0,win_s0[4]}};
@@ -1104,7 +1119,23 @@
   pri_win_best = {pri_s2[0], win_s2[0]};
   endfunction
   
-  wire clct_pri_win_best = pri_win_best(win_pri);
+  wire clct_pri_win_best = pri_win_best(win_pri[ 0],
+                                        win_pri[ 1],
+                                        win_pri[ 2],
+                                        win_pri[ 3],
+                                        win_pri[ 4],
+                                        win_pri[ 5],
+                                        win_pri[ 6],
+                                        win_pri[ 7],
+                                        win_pri[ 8],
+                                        win_pri[ 9],
+                                        win_pri[10],
+                                        win_pri[11],
+                                        win_pri[12],
+                                        win_pri[13],
+                                        win_pri[14],
+                                        win_pri[15]
+                                        );
   wire [3:0] clct_win_best = clct_pri_win_best[3:0];
   wire [3:0] clct_pri_best = clct_pri_win_best[7:4];
   //wire [3:0] clct_win_best = win_s2[0];
