586532252e96832cc7b6042e4045e9d04491479e
tahuang1991 <taohuang@phyics.tamu.edu>
2021-01-26 18:53:23 -0600

    update the GEM coordination converion into 1/8 strip level resolution

--- source/gem/cluster_to_cscwirehalfstrip.v
+++ source/gem/cluster_to_cscwirehalfstrip.v
@@ -26,7 +26,7 @@ module cluster_to_cscwirehalfstrip (
         output     [WIREBITS-1:0] cluster0_cscwire_lo,
         output     [WIREBITS-1:0] cluster0_cscwire_hi,
         output     [WIREBITS-1:0] cluster0_cscwire_mi,//middle
-        output     [MXXKYB-1:0]   cluster0_me1bxky_lo, // from 0-127
+        output     [MXXKYB-1:0]   cluster0_me1bxky_lo, // from 0-127 for halfstrip, later replaced by xky resolution
         output     [MXXKYB-1:0]   cluster0_me1bxky_hi, // from 0-127
         output     [MXXKYB-1:0]   cluster0_me1bxky_mi, // from 128-223, middle
         output     [MXXKYB-1:0]   cluster0_me1axky_lo, // from 128-223
@@ -64,11 +64,11 @@ parameter MAXKEYHSME1A = 10'd895;
 parameter ICLST        = 0;
 
 //reg [DATABITS-1:0] rom [ROMLENGTH-1:0];
-//reg [MXXKYB-1:0] me1a_hs_lo, me1a_hs_hi, me1b_hs_lo, me1b_hs_hi; 
+//reg [MXXKYB-1:0] me1a_xky_lo, me1a_xky_hi, me1b_xky_lo, me1b_xky_hi; 
 //reg [WIREBITS-1:0]  wire_lo, wire_hi;
 wire gem_clct_deltaxky = {gem_clct_deltahs, 2'b00};// convert HS level window to 1/8 strip level window
 
-wire [MXXKYB-1:0] me1a_hs_lo, me1a_hs_hi, me1b_hs_lo, me1b_hs_hi; 
+wire [MXXKYB-1:0] me1a_xky_lo, me1a_xky_hi, me1b_xky_lo, me1b_xky_hi; 
 wire [WIREBITS-1:0]  wire_lo, wire_hi;
 
 wire we = 0;
@@ -93,7 +93,7 @@ assign cluster0_pad_hi    = cluster0_vpf ? (cluster0_pad + cluster0_size) : 8'b0
  
 //GEM-CSC map, gempad to CSC keyhs
 
-gem_pad_to_csc_hs_lut upad_to_hs(
+gem_pad_to_csc_xky_lut upad_to_hs(
 
    .clock(logic_clock),
    .wen(we), // write enable
@@ -104,13 +104,13 @@ gem_pad_to_csc_hs_lut upad_to_hs(
    .reneven(evenchamber),
 
    .me1a_r_adr1  (cluster0_pad_lo), 
-   .me1a_r_data1 (me1a_hs_lo), 
+   .me1a_r_data1 (me1a_xky_lo), 
    .me1a_r_adr2  (cluster0_pad_hi), 
-   .me1a_r_data2 (me1a_hs_hi), 
+   .me1a_r_data2 (me1a_xky_hi), 
    .me1b_r_adr1  (cluster0_pad_lo), 
-   .me1b_r_data1 (me1b_hs_lo), 
+   .me1b_r_data1 (me1b_xky_lo), 
    .me1b_r_adr2  (cluster0_pad_hi), 
-   .me1b_r_data2 (me1b_hs_hi) 
+   .me1b_r_data2 (me1b_xky_hi) 
    );
 
 // GEM-CSC map, gem roll to CSC wire
@@ -138,25 +138,25 @@ reg [7:0]     reg_cluster0_pad; // from 0-191
 reg [2:0]     reg_cluster0_size; // from 0-7, 0 means 1 gem pad
 
 
-//`include "gem_pad_to_csc_hs_lut.v"
+//`include "gem_pad_to_csc_xky_lut.v"
 //`include "gem_roll_to_csc_wg_lut.v"
 
 
 always @(posedge logic_clock) begin
     //if (we)   begin
-    //    gem_pad_to_csc_hs_me1a_odd[0]   <= din;  // dummy write to help Xilinx infer a dual port block RAM 
-    //    gem_pad_to_csc_hs_me1a_even[0]  <= din;  // dummy write to help Xilinx infer a dual port block RAM 
-    //    gem_pad_to_csc_hs_me1b_odd[0]   <= din;  // dummy write to help Xilinx infer a dual port block RAM 
-    //    gem_pad_to_csc_hs_me1b_even[0]  <= din;  // dummy write to help Xilinx infer a dual port block RAM 
+    //    gem_pad_to_csc_xky_me1a_odd[0]   <= din;  // dummy write to help Xilinx infer a dual port block RAM 
+    //    gem_pad_to_csc_xky_me1a_even[0]  <= din;  // dummy write to help Xilinx infer a dual port block RAM 
+    //    gem_pad_to_csc_xky_me1b_odd[0]   <= din;  // dummy write to help Xilinx infer a dual port block RAM 
+    //    gem_pad_to_csc_xky_me1b_even[0]  <= din;  // dummy write to help Xilinx infer a dual port block RAM 
     //    gem_roll_to_csc_wg_odd_low  [0]       <= din2;
     //    gem_roll_to_csc_wg_odd_high [0]       <= din2;
     //    gem_roll_to_csc_wg_even_low [0]       <= din2;
     //    gem_roll_to_csc_wg_even_high[0]       <= din2;
     //end
-    //me1a_hs_lo   <=  (evenchamber ? gem_pad_to_csc_hs_me1a_even[cluster0_pad_lo] : gem_pad_to_csc_hs_me1a_odd[cluster0_pad_lo]);
-    //me1a_hs_hi   <=  (evenchamber ? gem_pad_to_csc_hs_me1a_even[cluster0_pad_hi] : gem_pad_to_csc_hs_me1a_odd[cluster0_pad_hi]);
-    //me1b_hs_lo   <=  (evenchamber ? gem_pad_to_csc_hs_me1b_even[cluster0_pad_lo] : gem_pad_to_csc_hs_me1b_odd[cluster0_pad_lo]);
-    //me1b_hs_hi   <=  (evenchamber ? gem_pad_to_csc_hs_me1b_even[cluster0_pad_hi] : gem_pad_to_csc_hs_me1b_odd[cluster0_pad_hi]);
+    //me1a_xky_lo   <=  (evenchamber ? gem_pad_to_csc_xky_me1a_even[cluster0_pad_lo] : gem_pad_to_csc_xky_me1a_odd[cluster0_pad_lo]);
+    //me1a_xky_hi   <=  (evenchamber ? gem_pad_to_csc_xky_me1a_even[cluster0_pad_hi] : gem_pad_to_csc_xky_me1a_odd[cluster0_pad_hi]);
+    //me1b_xky_lo   <=  (evenchamber ? gem_pad_to_csc_xky_me1b_even[cluster0_pad_lo] : gem_pad_to_csc_xky_me1b_odd[cluster0_pad_lo]);
+    //me1b_xky_hi   <=  (evenchamber ? gem_pad_to_csc_xky_me1b_even[cluster0_pad_hi] : gem_pad_to_csc_xky_me1b_odd[cluster0_pad_hi]);
 
     //wire_lo      <=  (evenchamber ? gem_roll_to_csc_wg_even_low [cluster0_roll] : gem_roll_to_csc_wg_odd_low [cluster0_roll]);
     //wire_hi      <=  (evenchamber ? gem_roll_to_csc_wg_even_high[cluster0_roll] : gem_roll_to_csc_wg_odd_high[cluster0_roll]);
@@ -174,26 +174,26 @@ wire [WIREBITS-1:0] wire_real_lo, wire_real_hi;
 assign wire_real_lo = (wire_lo < wire_hi) ? wire_lo : wire_hi; // in case of low and high values swapped
 assign wire_real_hi = (wire_lo > wire_hi) ? wire_lo : wire_hi; // in case of low and high values swapped
 
-wire [MXXKYB-1:0] me1a_hs_real_lo, me1a_hs_real_hi, me1b_hs_real_lo, me1b_hs_real_hi;
-assign me1a_hs_real_lo = (me1a_hs_lo < me1a_hs_hi) ? me1a_hs_lo : me1a_hs_hi;
-assign me1a_hs_real_hi = (me1a_hs_lo > me1a_hs_hi) ? me1a_hs_lo : me1a_hs_hi;
-assign me1b_hs_real_lo = (me1b_hs_lo < me1b_hs_hi) ? me1b_hs_lo : me1b_hs_hi;
-assign me1b_hs_real_hi = (me1b_hs_lo > me1b_hs_hi) ? me1b_hs_lo : me1b_hs_hi;
+wire [MXXKYB-1:0] me1a_xky_real_lo, me1a_xky_real_hi, me1b_xky_real_lo, me1b_xky_real_hi;
+assign me1a_xky_real_lo = (me1a_xky_lo < me1a_xky_hi) ? me1a_xky_lo : me1a_xky_hi;
+assign me1a_xky_real_hi = (me1a_xky_lo > me1a_xky_hi) ? me1a_xky_lo : me1a_xky_hi;
+assign me1b_xky_real_lo = (me1b_xky_lo < me1b_xky_hi) ? me1b_xky_lo : me1b_xky_hi;
+assign me1b_xky_real_hi = (me1b_xky_lo > me1b_xky_hi) ? me1b_xky_lo : me1b_xky_hi;
 
 // adding matching window
 assign cluster0_cscwire_lo  = (wire_real_lo > gem_alct_deltawire) ? wire_real_lo-gem_alct_deltawire : 7'd0;
 assign cluster0_cscwire_hi  = (wire_real_hi + gem_alct_deltawire < MAXWIRE) ? wire_real_hi+gem_alct_deltawire : 7'd47;
+assign cluster0_cscwire_mi = cluster0_cscwire_lo[WIREBITS-1:1] + cluster0_cscwire_hi[WIREBITS-1:1] + (cluster0_cscwire_lo[0] | cluster0_cscwire_hi[0]);
 
 //
-assign cluster0_me1bhs_lo   = (csc_cluster0_me1a) ? 10'd896 : ((me1b_hs_real_lo > MINKEYHSME1B+gem_clct_deltaxky) ? me1b_hs_real_lo-gem_clct_deltaxky : MINKEYHSME1B); //if not in Me1b region, give it an invalid value
-assign cluster0_me1bhs_hi   = (csc_cluster0_me1a) ? 10'd896 : ((me1b_hs_real_hi+gem_clct_deltaxky > MAXKEYHSME1B) ? MAXKEYHSME1B : me1b_hs_real_hi+gem_clct_deltaxky);
+assign cluster0_me1bxky_lo   = (csc_cluster0_me1a) ? 10'd896 : ((me1b_xky_real_lo > MINKEYHSME1B+gem_clct_deltaxky) ? me1b_xky_real_lo-gem_clct_deltaxky : MINKEYHSME1B); //if not in Me1b region, give it an invalid value
+assign cluster0_me1bxky_hi   = (csc_cluster0_me1a) ? 10'd896 : ((me1b_xky_real_hi+gem_clct_deltaxky > MAXKEYHSME1B) ? MAXKEYHSME1B : me1b_xky_real_hi+gem_clct_deltaxky);
 
-assign cluster0_me1ahs_lo   = (csc_cluster0_me1a) ? ((me1a_hs_real_lo > MINKEYHSME1A+gem_clct_deltaxky) ? me1a_hs_real_lo-gem_clct_deltaxky : MINKEYHSME1A) : 8'd224; //if not in Me1a region, give it an invalid value
-assign cluster0_me1ahs_hi   = (csc_cluster0_me1a) ? ((me1a_hs_real_hi+gem_clct_deltaxky > MAXKEYHSME1A) ? MAXKEYHSME1A : me1a_hs_real_hi+gem_clct_deltaxky) : 8'd224;
+assign cluster0_me1axky_lo   = (csc_cluster0_me1a) ? ((me1a_xky_real_lo > MINKEYHSME1A+gem_clct_deltaxky) ? me1a_xky_real_lo-gem_clct_deltaxky : MINKEYHSME1A) : 8'd224; //if not in Me1a region, give it an invalid value
+assign cluster0_me1axky_hi   = (csc_cluster0_me1a) ? ((me1a_xky_real_hi+gem_clct_deltaxky > MAXKEYHSME1A) ? MAXKEYHSME1A : me1a_xky_real_hi+gem_clct_deltaxky) : 8'd224;
 
-assign cluster0_cscwire_mi = cluster0_cscwire_lo[WIREBITS-1:1] + cluster0_cscwire_hi[WIREBITS-1:1] + (cluster0_cscwire_lo[0] | cluster0_cscwire_hi[0]);
-assign cluster0_me1ahs_mi  = cluster0_me1ahs_lo[MXXKYB-1:1] + cluster0_me1ahs_hi[MXXKYB-1:1] + (cluster0_me1ahs_lo[0] | cluster0_me1ahs_hi[0]);
-assign cluster0_me1bhs_mi  = cluster0_me1bhs_lo[MXXKYB-1:1] + cluster0_me1bhs_hi[MXXKYB-1:1] + (cluster0_me1bhs_lo[0] | cluster0_me1bhs_hi[0]);
+assign cluster0_me1axky_mi  = cluster0_me1axky_lo[MXXKYB-1:1] + cluster0_me1axky_hi[MXXKYB-1:1] + (cluster0_me1axky_lo[0] | cluster0_me1axky_hi[0]);
+assign cluster0_me1bxky_mi  = cluster0_me1bxky_lo[MXXKYB-1:1] + cluster0_me1bxky_hi[MXXKYB-1:1] + (cluster0_me1bxky_lo[0] | cluster0_me1bxky_hi[0]);
 
 assign csc_cluster0       = reg_cluster0;
 assign csc_cluster0_vpf   = reg_cluster0_vpf && gem_match_enable;// valid or not
