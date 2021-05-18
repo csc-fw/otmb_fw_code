9295a8e30cb3f73ae1d432be93eb52029943a986
tahuang1991 <taohuang@phyics.tamu.edu>
2021-05-18 15:15:16 -0500

    fix the bug

--- source/gem/gtx_gem_fiber_in.v
+++ source/gem/gtx_gem_fiber_in.v
@@ -42,8 +42,8 @@ module gtx_gem_fiber_in
     output reg [7:0]  k_char, 
     output            sump,
     output     [7:0]  errcount,
-    output     [15:0] notintablecount;
-    output     [15:0] disperrcount;
+    output     [15:0] notintablecount,
+    output     [15:0] disperrcount,
     output            link_had_err,
     output reg        link_good,
     output            link_bad
