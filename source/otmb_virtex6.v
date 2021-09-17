3dc4d4b27701a4639c65b0a839ec7b1e3819d1bf
tahuang1991 <taohuang@phyics.tamu.edu>
2021-09-17 18:01:17 -0500

    add counters for active cfeb flag from HMT and buffer stalled

--- source/otmb_virtex6.v
+++ source/otmb_virtex6.v
@@ -959,6 +959,8 @@
   wire  [MXCNTVME-1:0]  hmt_counter19;
   wire  [MXCNTVME-1:0]  hmt_trigger_counter;
   wire  [MXCNTVME-1:0]  hmt_readout_counter;
+  wire  [MXCNTVME-1:0]  hmt_aff_counter;
+  wire  [MXCNTVME-1:0]  buff_stall_counter;
 
 // GEM Counters
   wire  [MXCNTVME-1:0]  gem_counter0;
@@ -3830,6 +3832,8 @@ end
   .hmt_counter19 (hmt_counter19[MXCNTVME-1:0]),  // Out
   .hmt_trigger_counter (hmt_trigger_counter[MXCNTVME-1:0]), //Out
   .hmt_readout_counter (hmt_readout_counter[MXCNTVME-1:0]), //out
+  .hmt_aff_counter     (hmt_aff_counter[MXCNTVME-1:0]), //out
+  .buff_stall_counter  (buff_stall_counter[MXCNTVME-1:0]),
 
 
 // Sequencer Header Counters
@@ -6121,6 +6125,8 @@ wire [15:0] gemB_bxn_counter;
       .hmt_counter19 (hmt_counter19[MXCNTVME-1:0]),  //In
       .hmt_trigger_counter (hmt_trigger_counter[MXCNTVME-1:0]), //in
       .hmt_readout_counter (hmt_readout_counter[MXCNTVME-1:0]), //in
+      .hmt_aff_counter     (hmt_aff_counter[MXCNTVME-1:0]), //In
+      .buff_stall_counter  (buff_stall_counter[MXCNTVME-1:0]),//In
       
       // GEM Trigger/Readout Counter Ports
       .gem_cnt_all_reset    (gem_cnt_all_reset),     // Out  Trigger/Readout counter reset
--- source/vme/vme.v
+++ source/vme/vme.v
@@ -946,6 +946,8 @@
 
   hmt_trigger_counter,
   hmt_readout_counter,
+  hmt_aff_counter,
+  buff_stall_counter,
 
   gem_counter0,
   gem_counter1,
@@ -2719,6 +2721,8 @@
   input  [MXCNTVME-1:0]  hmt_counter19;
   input  [MXCNTVME-1:0]  hmt_trigger_counter;
   input  [MXCNTVME-1:0]  hmt_readout_counter;
+  input  [MXCNTVME-1:0]  hmt_aff_counter;
+  input  [MXCNTVME-1:0]  buff_stall_counter;
 
 // GEM Counters
   input  [MXCNTVME-1:0]  gem_counter0;
@@ -7401,7 +7405,7 @@
 //------------------------------------------------------------------------------------------------------------------
 // Remap 1D counters to 2D, because XST does not support 2D ports
   parameter MXHMTCNT = 20;
-  parameter MXCNT = 96 + MXHMTCNT + 2;                     // Number of counters, last counter id is mxcnt-1
+  parameter MXCNT = 100 + MXHMTCNT;                     // Number of counters, last counter id is mxcnt-1
   reg  [MXCNTVME-1:0] cnt_snap [MXCNT-1:0]; // Event counter snapshot 2D
   wire [MXCNTVME-1:0] cnt      [MXCNT-1:0]; // Event counter 2D map
 
@@ -7546,6 +7550,8 @@
 
   assign cnt[116] = hmt_trigger_counter;
   assign cnt[117] = hmt_readout_counter;
+  assign cnt[118] = hmt_aff_counter;// active cfeb flag from HMT
+  assign cnt[119] = buff_stall_counter;
 
 // Virtex-6 GTX Optical Receiver Error Counters
 //  assign cnt[81]  = gtx_rx_err_count0;  // Error count on this fiber channel
